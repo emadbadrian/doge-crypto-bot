fde738280f146ab545947fa6052327d6faa0ef34
emadbadrian <92640847+emadbadrian@users.noreply.github.com>
2025-05-09 20:37:40 +0330

    Update main.py

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ bot = Bot(token=TELEGRAM_BOT_TOKEN)
 
 # ======================= تابع دریافت داده ======================
 def fetch_doge_data():
-    url = f'https://api.coingecko.com/api/v3/coins/{SYMBOL}/market_chart?vs_currency={CURRENCY}&days=1&interval=daily'
+    url = f'https://api.coingecko.com/api/v3/coins/{SYMBOL}/market_chart?vs_currency={CURRENCY}&days=30&interval=hourly'
     response = requests.get(url)
     data = response.json()
 
